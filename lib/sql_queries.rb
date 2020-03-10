50805cdbc1cf90bb478fbb0df64ccccfd50c3e79
Kyle Talmage <ktalmage85@gmail.com>
2020-03-10 19:32:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT pledges.amount FROM pledges
-   JOIN products 
+   JOIN projects 
    ON projects.id = pledges.project_id
    ORDER BY projects.title ASC"
 end
